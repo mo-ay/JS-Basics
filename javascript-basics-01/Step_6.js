0fc88d5f90fb7d9134b8d5fb1683448d459cd9dd
mo-ay <mostafaaanezan@gmail.com>
2020-09-29 11:11:32 +0300

    Completed Step_6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,7 @@
+var but = document.getElementById("validate");
+but.addEventListener('click',()=>{
+    var fnum = document.getElementById("first_number").value;
+    var snum = document.getElementById("second_number").value;
+    window.alert(fnum / snum);
+
+});
