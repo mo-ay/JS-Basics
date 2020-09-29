092a6b8df635a706b3208da259ae1fa9d866d817
mo-ay <mostafaaanezan@gmail.com>
2020-09-29 10:32:15 +0300

    Completed Step_4

--- javascript-basics-01/Step_4.js
+++ javascript-basics-01/Step_4.js
@@ -1,4 +1,4 @@
 var name = window.prompt("Enter your name: ");
 var surname = window.prompt("Enter your surname: ");
-var city = window.prompt("Enter your city: ");
+var city = window.prompt("Enter your city : ");
 window.alert("hello" + "\n" + name + "\n" + surname + "\n" + city);
